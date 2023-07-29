163095207f99aea367ced606986812bdf072fbe3
MartinPDLE <131860719+MartinPDLE@users.noreply.github.com>
2023-07-29 02:55:47 +0200

    Finished folder structure

--- Master Thesis/article-disease-pred-with-kcde/inst/article-supplement/knit_article_supplement.R
+++
@@ -1,4 +0,0 @@
-setwd("/media/evan/data/Reich/infectious-disease-prediction-with-kcde/inst/article-supplement")
-library(knitr)
-knit2pdf("/media/evan/data/Reich/infectious-disease-prediction-with-kcde/inst/article-supplement/infectious-disease-prediction-with-kcde-SUPPLEMENT.Rnw")
-
--- Master Thesis/article-disease-pred-with-kcde/inst/article/knit_article.R
+++
@@ -1,3 +0,0 @@
-setwd("~/Documents/research/epi/kcde/article-disease-pred-with-kcde/inst/article")
-library(knitr)
-knit2pdf("~/Documents/research/epi/kcde/article-disease-pred-with-kcde/inst/article/infectious-disease-prediction-with-kcde.Rnw")
